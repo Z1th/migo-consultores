9269430d72dbb625888dc189d95e6ed1aacb687f
Programador 7 <programadorbt7@gmail.com>
2024-11-21 16:51:17 -0500

    Created Header, Hero and About Us components

---
+++ resources/views/components/about-us.blade.php
@@ -0,0 +1,33 @@
+<section class="p-0 wow animate__fadeIn bg-light-gray" id="services">
+    <div class="container-fluid">
+        <div class="row row-cols-1 row-cols-lg-2 row-cols-md-1">
+            <div
+                class="col padding-seven-half-tb padding-eight-lr xl-padding-five-half-tb xl-padding-six-lr lg-padding-five-all md-padding-ten-all sm-padding-15px-lr text-center text-lg-start last-paragraph-no-margin wow animate__fadeIn">
+                <span class="text-medium margin-15px-bottom d-block alt-font text-deep-pink">¿Quiénes Somos?</span>
+                <h4 class="alt-font text-extra-dark-gray font-weight-600 mx-auto mx-lg-0 sm-w-100">Despídete de los
+                    largos procesos de cobranza.</h4>
+                <p class="w-80 lg-w-100">
+                    Constituidos en la Ciudad de México el día 01 de
+                    agosto del 2019.</p>
+                    <p class="w-80 lg-w-100">
+                        Somos una firma de alta gama especializada en
+                        brindar servicios en resolucón de conflictos
+                        económicos, poniendo énfasis en la optimización
+                        de procesos de recuperación de activos y riesgo
+                        crediticio.
+                    </p>
+                    <p class="w-80 lg-w-100">
+                        Buscamos soluciones en la recuperación de cartera
+                        vencida para nuestros clientes.
+                    </p>
+                <a href="#"
+                    class="btn btn-dark-gray btn-small text-extra-small border-radius-4 margin-35px-top"><i
+                        class="fa-solid fa-circle-play icon-very-small margin-5px-right ms-0" aria-hidden="true"></i>
+                       Nuestros Servicios
+                    </a>
+            </div>
+            <div class="col position-relative md-h-500px sm-h-300 cover-background wow animate__fadeIn"
+                style="background-image: url('{{asset('assets/images/pexels-olly-859264.webp')}}')"></div>
+        </div>
+    </div>
+</section>
---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,55 @@
+<header>
+    <!-- start navigation -->
+    <nav class="navbar navbar-default bootsnav navbar-top header-dark background-transparent white-link navbar-expand-lg">
+        <div class="container-lg nav-header-container">
+            <!-- start logo -->
+            <div class="col-auto ps-0">
+                <a href="index.html" title="Logo Migo Consultores Jurídicos" class="logo">
+                    <img src="{{asset('assets/images/logos/migo-logo.webp')}}" alt="Logo Migo Consultores Jurídicos" class="logo-light default">
+                </a>
+            </div>
+            <!-- end logo -->
+            <div class="col accordion-menu pe-0 pe-md-3">
+                <button type="button" class="navbar-toggler collapsed" data-bs-toggle="collapse" data-bs-target="#navbar-collapse-toggle-1">
+                    <span class="sr-only">toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <div class="navbar-collapse collapse justify-content-end" id="navbar-collapse-toggle-1">
+                    <ul id="accordion" class="nav navbar-nav no-margin alt-font text-normal" data-in="animate__fadeIn" data-out="animate__fadeOut">
+                        <!-- start menu item -->
+                        <li class="dropdown megamenu-fw">
+                            <a href="/">Incio</a><i class="fa-solid fa-angle-down dropdown-toggle" data-bs-toggle="dropdown" aria-hidden="true"></i>
+                        </li>
+
+                        <li class="dropdown megamenu-fw">
+                            <a href="#nosotros">Nosotros</a><i class="fa-solid fa-angle-down dropdown-toggle" data-bs-toggle="dropdown" aria-hidden="true"></i>
+                        </li>
+
+                        <li class="dropdown megamenu-fw">
+                            <a href="#servicios">Servicios</a><i class="fa-solid fa-angle-down dropdown-toggle" data-bs-toggle="dropdown" aria-hidden="true"></i>
+                        </li>
+
+                        <li class="dropdown megamenu-fw">
+                            <a href="#cobertura">Cobertura</a><i class="fa-solid fa-angle-down dropdown-toggle" data-bs-toggle="dropdown" aria-hidden="true"></i>
+                        </li>
+
+                        <li class="dropdown megamenu-fw">
+                            <a href="#contacto">Contacto</a><i class="fa-solid fa-angle-down dropdown-toggle" data-bs-toggle="dropdown" aria-hidden="true"></i>
+                        </li>
+                     
+                    </ul>
+                </div>
+            </div>
+            <div class="col-auto pe-0">
+                <div class="header-social-icon d-none d-md-inline-block">
+                    <a href="#" title="Facebook" target="_blank"><i class="fa-brands fa-facebook-f" aria-hidden="true"></i></a>
+                    <a href="#" title="Twitter" target="_blank"><i class="fa-brands fa-twitter"></i></a>
+                    <a href="#" title="Dribbble" target="_blank"><i class="fa-brands fa-dribbble"></i></a>                          
+                </div>
+            </div>
+        </div>
+    </nav>
+    <!-- end navigation --> 
+</header>
