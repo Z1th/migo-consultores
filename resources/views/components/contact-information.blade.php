96940b7e77dee28d98d522b58f5678910a15ce1f
Programador 7 <programadorbt7@gmail.com>
2024-12-24 12:09:01 -0500

    Contact Information loaded

---
+++ resources/views/components/contact-information.blade.php
@@ -0,0 +1,22 @@
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-center margin-100px-bottom md-margin-50px-bottom sm-margin-30px-bottom">
+                <h4 class="font-weight-300 text-extra-dark-gray margin-15px-bottom">Mammut Interactive Corporate Design</h4>
+                <span class="text-deep-pink text-large border-bottom border-color-deep-pink font-weight-300">Branding Design &nbsp;+&nbsp;&nbsp; Magento Development &nbsp;+&nbsp;&nbsp; Mobile Website &nbsp;+&nbsp; eNewsletter</span>
+            </div>
+            <div class="col-lg-5 offset-lg-1 line-height-28 last-paragraph-no-margin md-margin-50px-bottom sm-margin-30px-bottom wow animate__fadeIn">
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages.</p>
+            </div>
+            <div class="col-lg-4 offset-lg-1 line-height-26 wow animate__fadeIn">
+                <ul class="list-style-6 margin-twelve-left">
+                    <li class="margin-5px-bottom"><label class="w-30 d-inline-block">Client:</label>Jason Richardson</li>
+                    <li class="margin-5px-bottom"><label class="w-30 d-inline-block">Industry:</label>Adventure  /  Travel</li>
+                    <li class="margin-5px-bottom"><label class="w-30 d-inline-block">Services:</label>Design, Art Direction, Website</li>
+                    <li class="margin-5px-bottom"><label class="w-30 d-inline-block">Date:</label>January 16th, 2017</li>
+                    <li class="margin-5px-bottom"><label class="w-30 d-inline-block">Website:</label><a href="https://1.envato.market/g7jrr" target="_blank">www.mammutadventure.com</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
---
+++ resources/views/components/contact-map.blade.php
@@ -0,0 +1,3 @@
+<section class="p-0 one-second-screen md-h-400px sm-h-300px wow animate__fadeIn">
+    <iframe class="w-100 h-100" src="{{ $src }}"></iframe>
+</section>
